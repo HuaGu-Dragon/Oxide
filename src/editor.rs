9e7e90559ef6fbf21bfe21999d9f314474b7674c
俺不叫化咕龙 <1801943622@qq.com>
2025-12-13 16:13:33 +0800

    Add `editor` to better handle input.
    Use `crossterm::read` to listen to input event.

---
+++ src/editor.rs
@@ -0,0 +1,31 @@
+use anyhow::Context;
+use crossterm::{
+    event::{Event, KeyCode, read},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn run(&self) -> anyhow::Result<()> {
+        enable_raw_mode().context("enable raw mode in terminal")?;
+
+        loop {
+            let event = read().context("read input")?;
+            println!("{event:?}");
+            if let Event::Key(e) = event
+                && let KeyCode::Char('q') = e.code
+            {
+                break;
+            }
+        }
+
+        disable_raw_mode().context("disable raw mode in terminal")?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use std::io::Read;
-
 use anyhow::Context;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+use crate::editor::Editor;
+
+mod editor;
 
 fn main() -> anyhow::Result<()> {
-    enable_raw_mode().context("enable raw mode in terminal")?;
-
-    for input in std::io::stdin().lock().bytes() {
-        let input = input.context("read input")?;
-        println!("Input: {}", input as char);
-        if input == b'q' {
-            disable_raw_mode().context("disable raw mode in terminal")?;
-            break;
-        }
-    }
+    let editor = Editor::new();
+    editor.run().context("running exide")?;
 
     Ok(())
 }
